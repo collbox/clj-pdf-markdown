ac145b55ac56acdd8ff88c39d02c0e16c7193424
Cameron Desautels <camdez@gmail.com>
2021-06-30 19:59:30 -0700

    Change group ID for fork, version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject clj-pdf-markdown "0.2.1"
-  :description "Library for rendering markdown to clj-pdf data-structure syntax."
-  :url "https://github.com/leontalbot/clj-pdf-markdown"
+(defproject org.clojars.camdez/clj-pdf-markdown "0.2.2-SNAPSHOT"
+  :description "Library for rendering markdown to clj-pdf data-structure syntax (CollBox fork)."
+  :url "https://github.com/collbox/clj-pdf-markdown"
   :license {:name "Eclipse Public License"
             :url "https://github.com/leontalbot/clj-pdf-markdown"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [com.atlassian.commonmark/commonmark "0.8.0"]])
-
